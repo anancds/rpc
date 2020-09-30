cbc7aefcf2f31cedc6bf555f84e682afa004113c
anancds <anancds@163.com>
2020-09-30 09:27:23 +0800

    updated event http

--- even-http/example.cpp
+++ even-http/example.cpp
@@ -2,16 +2,26 @@
 // Created by cds on 2020/9/29.
 //
 #include "event_http_server.h"
+#include <thread>
 
 using namespace Network;
 
+void StartHttpServer() {
+  EvHttpServ Serv("0.0.0.0", 8077);
+  Serv.RegistHandler("/hello", [](EvHttpResp *resp) { resp->QuickResponse(200, "Hello World!\n"); });
+  Serv.Start();
+}
 int main()
 {
-  EvHttpServ Serv("0.0.0.0", 8077);
+  std::unique_ptr<std::thread> http_server_thread_(nullptr);
+  http_server_thread_.reset(new std::thread(&StartHttpServer));
+  http_server_thread_->join();
 
-  Serv.RegistHandler("/hi/testget",  [](EvHttpResp *resp){
-    resp->QuickResponse(200,"Hello World!\n");});
-
-  Serv.Start();
+//  EvHttpServ Serv("0.0.0.0", 8077);
+//
+//  Serv.RegistHandler("/hi/testget",  [](EvHttpResp *resp){
+//    resp->QuickResponse(200,"Hello World!\n");});
+//
+//  Serv.Start();
   return 0;
 }
