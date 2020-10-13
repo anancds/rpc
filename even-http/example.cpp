66e31558a14a3747a1b7c69d6cb7124f65aa1c07
anancds <anancds@163.com>
2020-10-13 10:59:43 +0800

    updated

--- even-http/example.cpp
+++ even-http/example.cpp
@@ -4,10 +4,18 @@
 #include "event_http_server.h"
 #include <thread>
 #include <iostream>
+#include <regex>
 
 using namespace Network;
 using namespace std;
-
+bool CheckIp(const std::string &ip) {
+  std::regex pattern("((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+  std::smatch res;
+  if (regex_match(ip, res, pattern)) {
+    return true;
+  }
+  return false;
+}
 void StartHttpServer() {
   EvHttpServ Serv("0.0.0.0", 8077);
   Serv.RegistHandler("/hello", [](EvHttpResp *resp) { resp->QuickResponse(200, "Hello World!\n"); });
@@ -17,6 +25,7 @@ int main()
 {
   std::int16_t test = -1;
   std::cout << test << std::endl;
+  cout << CheckIp("0.0.0.0") << endl;
   std::unique_ptr<std::thread> http_server_thread_(nullptr);
   http_server_thread_.reset(new std::thread(&StartHttpServer));
   http_server_thread_->join();
